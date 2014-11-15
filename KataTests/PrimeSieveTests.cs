6eab9bbe4b84ac6fd22a9702db9a12bab5d27150
Emil Kantis <e.kantis@gmail.com>
2014-11-15 02:39:04 +0100

    Added a PrimeSieve project (first kata) and a Test project to practice some TDD as well

---
+++ KataTests/PrimeSieveTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace PrimeSieveTest
+{
+    public class PrimeSieveTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.Equal(4, 2+2);
+        }
+    }
+}
---
+++ PrimeSieve/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+// ---------------------------------------------------------------
+//         Algorithm description expressed in pseudocode 
+//  Taken from http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+// ---------------------------------------------------------------
+
+//    Input: an integer n > 1
+//     
+//    Let A be an array of Boolean values, indexed by integers 2 to n,
+//    initially all set to true.
+//     
+//     for i = 2, 3, 4, ..., not exceeding √n:
+//      if A[i] is true:
+//        for j = i2, i2+i, i2+2i, ..., not exceeding n :
+//          A[j] := false
+//     
+//    Output: all i such that A[i] is true.
+
+
+[<EntryPoint>]
+let main argv = 
+    let amountOfPrimes = argv.[0]
+    0
