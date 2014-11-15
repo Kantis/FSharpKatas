d364769cdba4c879a60f898c9230c40f1ed5bd68
Emil Kantis <e.kantis@gmail.com>
2014-11-15 11:29:19 +0100

    Chop chop!

---
+++ KarateChop/Chopper.fs
@@ -0,0 +1,23 @@
+﻿namespace KarateChop
+
+module Chopper =
+    let rec Chop (needle:int, haystack:int[]) : int =
+        if haystack = null || haystack.Length = 0 then
+            -1
+        else
+            let len = haystack.Length
+            let middle = len/2
+
+            if len = 1 then
+                if haystack.[0] = needle then
+                    0
+                else
+                    -1
+            else
+                if haystack.[middle] = needle then
+                    middle
+                else if haystack.[middle] < needle then
+                    middle + 1 + Chop(needle, haystack.[middle+1 .. len-1])
+                else
+                    Chop(needle, haystack.[0 .. middle-1])
+                    
---
+++ KataTests/KarateChopTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+using KarateChop;
+using Xunit;
+
+namespace PrimeSieveTest
+{
+    public class KarateChopTests
+    {
+        [Fact]
+        public void EmptyArray()
+        {
+            Assert.Equal(-1, Chopper.Chop(3, new int[] { }));
+        }
+
+        [Fact]
+        public void NotInArray()
+        {
+            Assert.Equal(-1, Chopper.Chop(3, new[] { 1 }));
+        }
+
+        [Fact]
+        public void LongArray()
+        {
+            int[] ints = new int[20];
+            for (var i = 0; i < 20; i++)
+            {
+                ints[i] = i;
+            }
+
+            foreach (var i in ints)
+            {
+                Assert.Equal(i, Chopper.Chop(i, ints));
+            }
+        }
+    }
+}
