e654f273f337974248a804e18f6096968fddd2fd
Emil Kantis <e.kantis@gmail.com>
2014-11-15 03:30:31 +0100

    Added another test. Satisfied with my siever for now.

--- KataTests/PrimeSieveTests.cs
+++ KataTests/PrimeSieveTests.cs
@@ -7,31 +7,41 @@ namespace PrimeSieveTest
         [Fact]
         public void TestZero()
         {
-            int[] actualPrimes = PrimeSieve.Siever.Sieve(0);
+            int[] sievedPrimes = PrimeSieve.Siever.Sieve(0);
 
             var expectedPrimes = new int[]{};
 
-            Assert.Equal(expectedPrimes, actualPrimes);
+            Assert.Equal(expectedPrimes, sievedPrimes);
         }
 
         [Fact]
         public void TestFirstPrime()
         {
-            int[] actualPrimes = PrimeSieve.Siever.Sieve(2);
+            int[] sievedPrimes = PrimeSieve.Siever.Sieve(2);
 
             var expectedPrimes = new int[] {2};
 
-            Assert.Equal(expectedPrimes, actualPrimes);
+            Assert.Equal(expectedPrimes, sievedPrimes);
         }
 
         [Fact]
         public void TestFirstFive()
         {
-            int[] actualPrimes = PrimeSieve.Siever.Sieve(11);
+            int[] sievedPrimes = PrimeSieve.Siever.Sieve(11);
 
             var expectedPrimes = new int[] {2, 3, 5, 7, 11};
 
-            Assert.Equal(expectedPrimes, actualPrimes);
+            Assert.Equal(expectedPrimes, sievedPrimes);
+        }
+
+        [Fact]
+        public void TestOneThousandthPrime()
+        {
+            int thousandthPrime = 7919;
+            int[] sievedPrimes = PrimeSieve.Siever.Sieve(10000);
+
+            Assert.True(sievedPrimes.Length >= 1000);
+            Assert.Equal(thousandthPrime, sievedPrimes[999]); // Account for zero-indexing
         }
     }
 }
